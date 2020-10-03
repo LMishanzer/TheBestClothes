322401c03a18046626f9567d8d82fc86b1b71413
Mike Leskiv <leskivmisha@gmail.com>
2020-10-03 15:01:16 +0200

    Add project files.

---
+++ TheBestClothes/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBestClothes.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public DateTime VisitDateTime { get; set; }
+        public int Age { get; set; }
+        public bool IsSatisfied { get; set; }
+        public char Sex { get; set; }
+    }
+}
